ede858950ecfc747fe1d703702c0151a11912c3a
Henrique Monteverde <henrique.monteverde@seazone.com.br>
2026-03-27 16:26:22 -0300

    allow blob images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@resvg/resvg-js"],
+  images: {
+    domains: ["qrapd3qjiankddu3.public.blob.vercel-storage.com"],
+  },
 };
 
 export default nextConfig;
